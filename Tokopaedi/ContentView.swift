d95d5cae97f668edb62ed5c8a8f204efec1e8feb
Rayhan Faluda <rayhanfaluda@gmail.com>
2021-07-09 15:08:43 +0700

    Initial Commit

---
+++ Tokopaedi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tokopaedi
+//
+//  Created by Rayhan Martiza Faluda on 09/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tokopaedi/TokopaediApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokopaediApp.swift
+//  Tokopaedi
+//
+//  Created by Rayhan Martiza Faluda on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TokopaediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
